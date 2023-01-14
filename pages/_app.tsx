505c52a2c05d4d779b18470fe430d51b6207671c
Derek Reiff <d@derekr.net>
2023-01-14 11:03:45 -0800

    Add analytics before moving on

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import PlausibleProvider from "next-plausible";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <PlausibleProvider
+            domain="sno-park.site"
+            selfHosted={true}
+            customDomain="https://io.fun-club.xyz"
+        >
+            <Component {...pageProps} />
+        </PlausibleProvider>
+    );
 }
